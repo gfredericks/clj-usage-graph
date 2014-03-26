b61be6150cf0b9676b13ff097a9a0a29082448b1
Gary Fredericks <fredericksgary@gmail.com>
2014-03-26 10:48:05 -0700

    Move the graphviz generation stuff into its own namespace

--- src/com/gfredericks/clj_usage_graph.clj
+++ src/com/gfredericks/clj_usage_graph.clj
@@ -1,18 +1,8 @@
 (ns com.gfredericks.clj-usage-graph
   "Entry point."
   (:refer-clojure :exclude [munge])
-  (:require [clojure.string :as s]
+  (:require [com.gfredericks.clj-usage-graph.graphviz :as graphviz]
             [com.gfredericks.clj-usage-graph.usages :refer [all-internal-usages]]))
-
-(defn munge
-  "Transform characters that graphviz won't want in node identifiers."
-  [s]
-  (s/replace s #"[^A-Za-z0-9_]"
-             (fn [s']
-               (-> s'
-                   first
-                   int
-                   (->> (format "_%d_"))))))
 
 (defn generate
   "Given files to analyze, prints a graph file for DOT."
@@ -22,27 +12,4 @@
   ;; the files here is necessary, but the current code doesn't work
   ;; otherwise
   (doseq [f filenames] (load-file f))
-
-  (let [m (all-internal-usages filenames)
-        namespace #(str (.ns %))
-        name #(str (.sym %))
-        id (fn [v]
-             (munge (str (namespace v) \. (name v))))
-        namespaces (->> (keys m)
-                        (map namespace)
-                        (distinct))
-        in-ns #(->> (keys m)
-                    (filter (comp #{%} namespace)))]
-    (println "digraph G {")
-    (println "rankdir=\"LR\";")
-    (println "fontname=\"monospace\";")
-    (println "node[fontname=\"monospace\"];")
-    (doseq [ns namespaces]
-      (printf "subgraph cluster_%s {\nshape=\"rect\";\nlabel=\"%s\";\n" (munge ns) ns)
-      (doseq [v (in-ns ns)]
-        (printf "%s[label=\"%s\"];\n" (id v) (name v)))
-      (println "}"))
-    (doseq [[v vs] m
-            v' vs]
-      (printf "%s -> %s;\n" (id v) (id v')))
-    (println "}")))
+  (graphviz/generate (all-internal-usages filenames)))
---
+++ src/com/gfredericks/clj_usage_graph/graphviz.clj
@@ -0,0 +1,39 @@
+(ns com.gfredericks.clj-usage-graph.graphviz
+  "Generating graphviz output from a usage graph."
+  (:require [clojure.string :as s]))
+
+(defn munge
+  "Transform characters that graphviz won't want in node identifiers."
+  [s]
+  (s/replace s #"[^A-Za-z0-9_]"
+             (fn [s']
+               (-> s'
+                   first
+                   int
+                   (->> (format "_%d_"))))))
+
+(defn generate
+  "Given a map from vars to sets of vars, prints the graphviz output."
+  [m]
+  (let [namespace #(str (.ns %))
+        name #(str (.sym %))
+        id (fn [v]
+             (munge (str (namespace v) \. (name v))))
+        namespaces (->> (keys m)
+                        (map namespace)
+                        (distinct))
+        in-ns #(->> (keys m)
+                    (filter (comp #{%} namespace)))]
+    (println "digraph G {")
+    (println "rankdir=\"LR\";")
+    (println "fontname=\"monospace\";")
+    (println "node[fontname=\"monospace\"];")
+    (doseq [ns namespaces]
+      (printf "subgraph cluster_%s {\nshape=\"rect\";\nlabel=\"%s\";\n" (munge ns) ns)
+      (doseq [v (in-ns ns)]
+        (printf "%s[label=\"%s\"];\n" (id v) (name v)))
+      (println "}"))
+    (doseq [[v vs] m
+            v' vs]
+      (printf "%s -> %s;\n" (id v) (id v')))
+    (println "}")))
