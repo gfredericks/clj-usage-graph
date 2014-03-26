fff2cef00f7053308de568be60a0b6215fc006f1
Brian Rowe <browe@rentpath.com>
2014-03-26 13:09:06 -0400

    Add a source tree for development code.

--- project.clj
+++ project.clj
@@ -5,4 +5,5 @@
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.6.0"]
                  [com.datomic/datomic-free "0.9.4699"]
-                 [org.clojure/tools.analyzer.jvm "0.1.0-beta8"]])
+                 [org.clojure/tools.analyzer.jvm "0.1.0-beta8"]]
+  :profiles {:dev {:source-paths ["dev"]}})
