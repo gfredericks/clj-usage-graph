3e2f034b04e3d5a9e2d61aa542c68fc29d190db3
Gary Fredericks <fredericksgary@gmail.com>
2015-03-11 14:43:50 -0500

    Version 0.2.1-SNAPSHOT

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject com.gfredericks/clj-usage-graph "0.2.0"
+(defproject com.gfredericks/clj-usage-graph "0.2.1-SNAPSHOT"
   :description "Haxy usage-graph generation code for Clojure projects"
   :url "https://github.com/fredericksgary/clj-usage-graph"
   :license {:name "Eclipse Public License"
