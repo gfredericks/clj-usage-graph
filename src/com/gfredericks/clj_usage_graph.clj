2b54355f5928c15963cccc302173b7f16bf9f3fa
Gary Fredericks <fredericksgary@gmail.com>
2015-05-15 10:12:28 -0500

    Proof of concept code for namespace graphs

--- src/com/gfredericks/clj_usage_graph.clj
+++ src/com/gfredericks/clj_usage_graph.clj
@@ -1,7 +1,9 @@
 (ns com.gfredericks.clj-usage-graph
   "Entry point."
   (:refer-clojure :exclude [munge])
-  (:require [clojure.string :as s]
+  (:require [clojure.java.io :as io]
+            [clojure.string :as s]
+            [clojure.tools.namespace.parse :as ctnp]
             [com.gfredericks.clj-usage-graph.usages :refer [all-internal-usages]]))
 
 (defn munge
@@ -39,4 +41,27 @@
     (doseq [[v vs] m
             v' vs]
       (printf "%s -> %s;\n" (id v) (id v')))
+    (println "}")))
+
+(defn generate-namespace-graph
+  [& filenames]
+  (let [names&deps (for [filename filenames
+                         :let [decl (-> filename
+                                        io/reader
+                                        java.io.PushbackReader.
+                                        ctnp/read-ns-decl)
+                               deps (ctnp/deps-from-ns-decl decl)]]
+                     [(second decl) deps])
+        node-id (memoize (fn [_] (gensym "node")))
+        included? (set (map first names&deps))]
+    (println "digraph G {")
+    (println "ranksep=2")
+    (println "concentrate=true;")
+    (println "fontname=\"monospace\";")
+    (println "node[fontname=\"monospace\"];")
+    (doseq [[ns-name deps] names&deps]
+      (printf "%s[label=\"%s\"];\n" (node-id ns-name) ns-name)
+      (doseq [dep deps
+              :when (included? dep)]
+        (printf "%s -> %s;\n" (node-id ns-name) (node-id dep))))
     (println "}")))
